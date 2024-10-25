957fe9f35cc2cb724a38d186b6e624808aaacb95
Stasik-k <159716434+Stasik-k@users.noreply.github.com>
2024-10-25 21:30:12 +0300

    Update Footer.tsx

--- src/Components/Layout/Footer/Footer.tsx
+++ src/Components/Layout/Footer/Footer.tsx
@@ -1,15 +1,30 @@
-import { Footer } from "flowbite-react";
+import { Footer, FooterLink } from "flowbite-react";
+import { FaHeart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { TRootState } from "../../../Store/BigPie";
+import { Link } from "react-router-dom";
+import { IoCardSharp } from "react-icons/io5";
 
 
 
 const Footer1 = () => {
+  const user = useSelector((state: TRootState) => state.UserSlice.user);
+
   return (
     <Footer container
       className="flex flex-col items-center justify-center"
     >
-      <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-      <Footer.LinkGroup>
+      <Footer.LinkGroup className="gap-5">
+        <FooterLink className="mt-[13px]" href="/about">About</FooterLink>
 
+        {user && (
+          <>
+            <Link to="/mycards"><IoCardSharp style={{ margin: '0 23px' }} />My Cards</Link>
+            <Link to="/favorites"><FaHeart style={{ margin: '0 20px', color: 'red' }} />Favorites</Link>
+
+
+          </>
+        )}
       </Footer.LinkGroup>
     </Footer>
   );
